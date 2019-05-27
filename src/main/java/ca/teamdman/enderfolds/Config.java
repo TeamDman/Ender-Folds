df2d8136ca062126638f347c81fb0a58d0fcc61f
TeamDman <TeamDman9201@gmail.com>
2019-05-26 21:53:29 -0400

    Fix ConfigFactory not saving

--- src/main/java/ca/teamdman/enderfolds/Config.java
+++ src/main/java/ca/teamdman/enderfolds/Config.java
@@ -1,6 +1,12 @@
 package ca.teamdman.enderfolds;
 
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
 @net.minecraftforge.common.config.Config(modid= EnderFolds.MOD_ID, name= EnderFolds.MOD_NAME, category="")
+@Mod.EventBusSubscriber(modid = EnderFolds.MOD_ID)
 public class Config {
 	@net.minecraftforge.common.config.Config.Comment("General Options")
 	@net.minecraftforge.common.config.Config.RequiresMcRestart
@@ -10,5 +16,11 @@ public class Config {
 		public boolean indestructibleDestinations = false;
 		@net.minecraftforge.common.config.Config.Comment("Only allows one core to be extracted from a destination.")
 		public boolean unqiueCores = true;
+	}
+
+	@SubscribeEvent
+	public static void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent event) {
+		if (event.getModID().equals(EnderFolds.MOD_ID))
+			ConfigManager.sync(event.getModID(), net.minecraftforge.common.config.Config.Type.INSTANCE);
 	}
 }
